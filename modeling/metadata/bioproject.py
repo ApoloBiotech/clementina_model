03108a7f98df277113d08ed6488e9a29520148f6
maximo rivarola <90464007+rivabros2@users.noreply.github.com>
2025-01-24 16:51:06 -0300

    Add files via upload

---
+++ modeling/metadata/bioproject.py
@@ -0,0 +1,46 @@
+import requests, re
+
+def get_internal_id(accession):
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=bioproject&term={accession}"
+    res = requests.get(api_url)
+    return re.search(r"<Id>(\d+)</Id>", res.text).group(1)
+
+def get_pmid(doi):
+    api_url = f"https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=tesis_guido_freire&email=example@outlook.com&ids={doi}"
+    res = requests.get(api_url)
+    pmid = re.search(r'pmid="(\d+)"', res.text)
+    if pmid:
+        return pmid.group(1)
+    return None
+
+def get_bioproject(bioproj_id):
+    internal_id = get_internal_id(bioproj_id)
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=bioproject&id={internal_id}"
+    res = requests.get(api_url)
+    return res.text
+
+def from_bioproject(metadata):
+    res = {}
+    name = re.search(r"<Name>(.+)</Name>", metadata)
+    if name:
+        res['name'] = name.group(1)
+    title = re.search(r"<Title>(.+)</Title>", metadata)
+    if title:
+        res['title'] = title.group(1)
+    description = re.search(r"<Description>(.+)</Description>", metadata)
+    if description:
+        res['description'] = description.group(1)
+    doi = re.search(r'<Publication[^>]*\sid="([^"]+)"', metadata)
+    if doi:
+        res['doi'] = doi.group(1)
+
+    return res
+
+def get_abstract(doi):
+    pmid = get_pmid(doi)
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pmid}"
+    res = requests.get(api_url)
+    abstract = re.search(r'<AbstractText>(.+)</AbstractText>', res.text)
+    if abstract:
+        return abstract.group(1)
+    return None
---
+++ modeling/metadata/biosample.py
@@ -0,0 +1,39 @@
+import re, requests
+
+def get_internal_id(accession):
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=biosample&term={accession}"
+    res = requests.get(api_url)
+    try:
+        res = re.search(r"<Id>(\d+)</Id>", res.text).group(1)
+        return res
+    except:
+        return accession
+
+def get_biosample(biosamp_id):
+    internal_id = get_internal_id(biosamp_id)
+    api_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=biosample&id={internal_id}&rettype=full&retmode=text"
+    res = requests.get(api_url)
+    return res.text
+
+def from_biosample(metadata):
+    res = {}
+    res['title'] = metadata.split('\n')[0].replace('1: ', '')
+    identifiers = re.search(r'Identifiers: (.+)', metadata)
+    if identifiers:
+        res['Identifiers'] = {k.strip(): v.strip() for k, v in 
+                                [item.split(':') if ':' in item else (item, "")
+                                    for item in identifiers.group(1).split(';')]}
+    organism = re.search(r'Organism: (.+)', metadata)
+    if organism:
+        res['Organism'] = organism.group(1).strip()
+    attributes = re.findall(r'/(\w+)="([^"]+)"', metadata)
+    if attributes:
+        res['Attributes'] = {k: v for k, v in attributes}
+    accession = re.search(r'Accession: (\w+)', metadata)
+    if accession:
+        res['Accession'] = accession.group(1)
+    id_match = re.search(r'ID: (\d+)', metadata)
+    if id_match:
+        res['ID'] = id_match.group(1)
+    
+    return res
